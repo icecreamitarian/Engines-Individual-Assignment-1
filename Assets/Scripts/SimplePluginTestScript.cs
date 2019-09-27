ef0c24477df96cbe00ebc77792f938b94356a0f3
Josh Wessel <joshua.wessel@ontariotechu.net>
2019-09-27 12:08:42 -0400

    Initial commit

---
+++ Assets/Scripts/SimplePluginTestScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+public class SimplePluginTestScript : MonoBehaviour
+{
+    const string DLL_NAME = "SimplePluginTest";
+    [DllImport(DLL_NAME)]
+    private static extern int SimpleFunction();
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log(SimpleFunction());
+        }
+    }
+}
---
+++ Assets/Scripts/objectMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectMovement : MonoBehaviour
+{
+    public float speed = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        gameObject.transform.position = new Vector2
+           (transform.position.x + (horizontal * speed),
+           transform.position.y + (vertical * speed));
+    }
+}
+
+// Code loosely based on code from https://www.tutorialspoint.com/unity/unity_basic_movement_scripting.htm.
